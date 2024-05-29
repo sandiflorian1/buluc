c244f3cc9e68eb5d5160d62ca3f5e34eb825e002
Alexandra Florian <sandi.florian@yahoo.com>
2024-05-29 15:37:38 +0300

    Update experience page

--- src/components/animations/Animations.tsx
+++ src/components/animations/Animations.tsx
@@ -1,4 +1,5 @@
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+import { useRef } from 'react';
 
 const FadeInViewPortAnimation = ({ children, className, ...props }: any) => {
     return (
@@ -15,14 +16,45 @@ const FadeInViewPortAnimation = ({ children, className, ...props }: any) => {
     );
   };
    
-  const ScaleViewPortAnimation = ({ children, className, ...props }: any) => {
+const ScaleViewPortAnimation = ({ children, className, ...props }: any) => {
+  return (
+    <motion.div
+      className={className}
+      initial={{ scale: 0 }}
+      whileInView={{ scale: 1 }}
+      viewport={{ once: true, amount: 0.4 }}
+      transition={{ duration: 0.5 }}
+      {...props}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+const SlideViewPortAnimation = ({ children, className, direction, ...props }: any) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.4 });
+  const cardVariants = {
+    hidden: {
+      opacity: 0,
+      ...{ y: direction === 'up' ? 300 : -300 },
+    },
+    visible: {
+      opacity: 1,
+      ...{ y: 0 },
+      transition: {
+        delay: 0.2,
+        duration: 0.5,
+      },
+    },
+  };
     return (
       <motion.div
         className={className}
-        initial={{ scale: 0 }}
-        whileInView={{ scale: 1 }}
-        viewport={{ once: true, amount: 0.4 }}
-        transition={{ duration: 0.5 }}
+        ref={ref}
+        variants={cardVariants}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
         {...props}
       >
         {children}
@@ -30,4 +62,4 @@ const FadeInViewPortAnimation = ({ children, className, ...props }: any) => {
     );
   };
 
-export { FadeInViewPortAnimation, ScaleViewPortAnimation }
+export { FadeInViewPortAnimation, ScaleViewPortAnimation, SlideViewPortAnimation }
--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,11 +1,13 @@
 import MainLayout from "../components/layouts/MainLayout";
 import { useRef, useEffect, Ref } from 'react';
 import { gsap } from "gsap";
+import TitleBanner from "../components/layouts/TitleBanner";
+import BannerTextAbsolute from "../components/layouts/BannerTextAbsolute";
 import IMAGES from "../assets/Images";
+import { FadeInViewPortAnimation, SlideViewPortAnimation } from "../components/animations/Animations";
+
 
 export interface IExperienceProps { }
-
-
 const Experience: React.FC = () => {
   const experienceRef = useRef(null);
 
@@ -19,17 +21,159 @@ const Experience: React.FC = () => {
   });
 
   return (
-    <MainLayout title="@The Experience">
-      <section id="experience" ref={experienceRef}>
-        <div>
-          <div className="flex flex-col md:flex-row mx-auto py-8 md:py-8" style={{ maxWidth: '50vw' }}>
-            <div className="flex flex-col text-center">
-              <p className="mb-1 text-5xl md:text-xl text-center">Challenge Yourself @The experience este un concept ce aduce în sufrageria de pe Madrid 4  un curs cu apucături de experience. Mai bine zis o ședință în care ne “jucăm” exact ca la un curs obișnuit doar ca adăugăm atmosfera de experience în sufragerie (evident), dar nu oricum ci în pijamale. </p>
-              <p className="mb-1 text-5xl md:text-xl text-center">Ședința include 2h30 de jocuri, distracție & socializare toate într-o atmosferă de party realizate prin intermediul unor tehnici teatrale ce te vor ajuta să-ți antrenezi creativitatea, viteza de reacție și te vor învăța să lucrezi cu emoțiile. </p>
-            </div>
+    <MainLayout noPadding>
+      <FadeInViewPortAnimation>
+        <TitleBanner
+          title="@The Experience"
+          text={"The Challenge Yourself @EXPERIENCE - este o călătorie artistică unde te vei conecta cu sinele tău într-un spațiu safe, unde natura devine nu doar o sursă de inspirație, ci și un partener în procesul creativ. Este o creștere interioară ce pleacă de la prima activitate de învățare a omului - JOCUL. Timp de 4 zile explorăm diverse forme de exprimare artistică: de la mișcarea corporală, la teatru, de la depășirea barierelor, la joacă - toate sub forma unei competiții."}
+          bgBluredImg={IMAGES.Experience1}
+          bgImg={IMAGES.Experience2}
+          colorText="white"
+        />
+      </FadeInViewPortAnimation>
+
+      <div className="mx-[12%] mb:mx-6">
+        <BannerTextAbsolute
+          imageURL={IMAGES.Experience4}
+          direction="left"
+          className="h-[35vw] pb-20"
+          title="Ce am pregătit?"
+        >
+          <p className="">
+            Competitivitatea stă la baza acestei experiențe și te va teleporta direct într-un bootcamp artistic careva îmbina relaxarea cu ateliere de expresie corporală, teatru, activități ce presupun depășirea barierelor, și multă joacă Pe tot parcursul probelor & atelierelor vei aduna puncte care vor conta la finalul călătoriei. Pregătim multe surprize și un podium care abia așteaptă să fie cucerit.
+          </p>
+        </BannerTextAbsolute>
+      </div>
+
+      <div className="mx-[12%] mb:mx-6">
+        <FadeInViewPortAnimation className="grid grid-cols-6 mb:grid-cols-1 gap-8 pb-20">
+          <div className="col-start-1 col-end-3 mb:col-end-7">
+            <h4 className="title pb-4 text-red">
+              Pentru cine este?
+            </h4>
+            <ul className="list-disc pl-8">
+              <li>pentru cei care competiția este ghidată de adrenalină</li>
+              <li>pentru cei care iubesc să-și depășească limitele </li>
+              <li>pentru cei care au experimentat sau nu cursul Challenge Yourself </li>
+              <li>pentru tine, dacă îți este dor să te joci</li>
+            </ul>
           </div>
+
+          <div className="col-end-7 col-span-4 mb:col-span-7">
+            <h4 className="title pb-4 text-red">
+              CE ESTE INCLUS
+            </h4>
+
+            <ul className="list-disc pl-8 grid grid-cols-2 gap-x-10">
+              <li>un sat doar pt tine - exclusivitate în locație</li>
+              <li>curte de 5000m2</li>
+              <li>3 nopți de cazare (1 - august)</li>
+              <li>3 mese pe zi 100% homemade</li>
+              <li>apa de izvor & ceaiuri nelimitat pe timpul zilei</li>
+              <li>3 cafele de persoana/ zi - cafea de specialitate</li>
+              <li>acces la saună</li>
+              <li>vin & Prosecco</li>
+              <li>transport dus - întors</li>
+              <li>goodies & premii de la parteneri</li>
+            </ul>
+
+          </div>
+        </FadeInViewPortAnimation>
+      </div>
+
+      <div className="mx-[12%] mb:mx-6">
+        <BannerTextAbsolute
+          imageURL={IMAGES.Experience5}
+          direction="right"
+          className="h-[35vw] pb-20"
+          title="YOGA CU MARTA"
+        >
+          <p className="">
+            Alături de Martha ne vom bucura de yoga la apus, unde ne conectăm la respirație, ne ghidăm armonios spre o stare de spirit de liniște, pace, prezență. Vom termina cu o meditatie ghidata sau chiar yoga Nidra (Yoga Nidra, cunoscută și sub numele de somn yoghin) este o meditație ghidată extrem de benefică. Clasa este accesibilă tuturor nivelelor de practică.
+          </p>
+        </BannerTextAbsolute>
+      </div>
+
+      <div className="mx-[12%] mb:mx-6 pb-20">
+        <div className="columns-2 gap-10">
+          <SlideViewPortAnimation direction="up" className="pb-6">
+            <h4 className="text-orange">Drumeție cu liv tu visit</h4>
+            <p>Împreună cu LIV TU VISIT  - creatori de explorări urbane și experiențe turistice inedite - vom parcurge un traseu pe munții Coziei - cu istorie & legende.</p>
+          </SlideViewPortAnimation>
+          <SlideViewPortAnimation><img src={IMAGES.Experience6}></img></SlideViewPortAnimation>
+          <SlideViewPortAnimation><img src={IMAGES.Experience6}></img></SlideViewPortAnimation>
+          <SlideViewPortAnimation className="pt-6">
+            <h4 className="text-orange">Foc de tabără</h4>
+            <p>Ne strângem buluc seara, la foc de tabără, în jurul poveștilor sub cerul înstelat și sub privirile atente ale vârfului Cozia. Un moment de de conectare și de regăsire a simplității și a frumuseții vieții chiar la poalele muntelui.</p>
+          </SlideViewPortAnimation>
         </div>
-      </section>
+      </div>
+
+      <div className="mx-[12%] mb:mx-6">
+        <BannerTextAbsolute
+          imageURL={IMAGES.Experience8}
+          direction="left"
+          className="h-[40vw] pb-20"
+          title="Despre albastru și origini"
+        >
+          <p className="pb-4">
+            Un sat din Berislăvești, Jud. Vâlcea format din 4 case vechi de peste 100 de ani srămutate și readuse la viață, din nordul țării, alături de un hambar transformat în vatra satului – locul unde ne vom aduna buluc cu toții la povesti si la bucate delicioase. Un loc în care timpul parcă stă în loc.
+          </p>
+          <ul className="list-disc pl-8">
+            <li>Casa Amurg</li>
+            <li>Casa Zori de Zi</li>
+            <li>Casa Rădăcini</li>
+            <li>Casa Origini</li>
+            <li>Hambarul Albastru și Origini</li>
+          </ul>
+        </BannerTextAbsolute>
+      </div>
+
+      <div className="mx-[12%] mb:mx-6">
+        <BannerTextAbsolute
+          imageURL={IMAGES.Experience7}
+          direction="right"
+          className="h-[40vw] pb-20"
+          title="BUCĂTĂRIA"
+        >
+          <p className="pb-4">
+            Te vei bucura de o bucatarie cu mult bun gust și de influențe din multe culturi. Și, mai ales, 100% homemade. 3 mese pe zi  gătite “with a twist”, niciodată banal. O mancare…eclectică, așa cum este și satul Albastru și Origini.
+          </p>
+          <p className="pb-4">
+            Vei lua masa într-o șură veche, transformată în bucatarie, șezătoare, sufragerie, loc de povesti, loc de adunări buluc …și dacă ești fan mâncării vegetariene - te vei bucura de legume de sezon.
+          </p>
+        </BannerTextAbsolute>
+      </div>
+
+      <div className="mx-[12%] mb:mx-6 pb-20">
+        <h4 className="title pb-4 text-orange">
+          ECHIPA
+        </h4>
+        <div className="columns-4 mb:columns-1 gap-10">
+          <SlideViewPortAnimation direction="up" className="pb-4"><p className="text-orange text-xl mb:text-5xl text-center">Mella - trainer </p></SlideViewPortAnimation>
+          <SlideViewPortAnimation><img src={IMAGES.echipaMihaelaMardare}></img></SlideViewPortAnimation>
+          <SlideViewPortAnimation><img src={IMAGES.echipaRobertCiupitu}></img></SlideViewPortAnimation>
+          <SlideViewPortAnimation direction="up" className="pt-4"><p className="text-red text-xl mb:text-5xl text-center">Robert - trainer </p></SlideViewPortAnimation>
+          <SlideViewPortAnimation direction="up" className="pb-4"><p className="text-orange text-xl mb:text-5xl text-center">Mari - comunicare & social media  </p></SlideViewPortAnimation>
+          <SlideViewPortAnimation><img src={IMAGES.echipaMarilenaAdam}></img></SlideViewPortAnimation>
+          <SlideViewPortAnimation><img src={IMAGES.echipaCezarStoica}></img></SlideViewPortAnimation>
+          <SlideViewPortAnimation direction="up" className="pt-4"><p className="text-red text-xl mb:text-5xl text-center">Cezar - identitate vizuală</p></SlideViewPortAnimation>
+        </div>
+      </div>
+
+      <FadeInViewPortAnimation className="mx-[12%] mb:mx-6 pb-20">
+        <h4 className="title pb-4 text-orange">
+          Cum mă înscriu?
+        </h4>
+        <p>Înscrierea se face exclusiv prin email la rezervari.buluc@gmail.com</p>
+      </FadeInViewPortAnimation>
+
+      <FadeInViewPortAnimation className="mx-[12%] mb:mx-6 pb-20">
+        <h1 className="title pb-4 text-orange text-center">
+          LOCURI LIMITATE
+        </h1>
+      </FadeInViewPortAnimation>
+
     </MainLayout>
   );
 };
