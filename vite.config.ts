03c119b56f98f791e10f4de6f125454785deee8f
Alexandra Florian <sandi.florian@yahoo.com>
2024-08-06 19:19:50 +0300

    fix redirects

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { copyFileSync } from 'fs';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
+
+const copyRedirects = () => {
+  return {
+    name: 'copy-redirects',
+    closeBundle: () => {
+      copyFileSync(resolve(__dirname, '_redirects'), resolve(__dirname, 'dist', '_redirects'));
+    }
+  }
+}
+
 export default defineConfig({
   plugins: [
     react(),
+    copyRedirects()
   ],
+  build: {
+    outDir: 'dist',
+  },
   assetsInclude: ['**/*.gltf'],
 })
